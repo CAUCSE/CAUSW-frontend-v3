b8e04f6e16615c977a349cbb43dc2ff5cbea4d23
kyh0726 <younghyun75398@gmail.com>
2026-01-29 15:38:54 +0900

    feat: req 인터셉터 단에서 fetch revalidate 기본 옵션 주입

--- apps/web/src/shared/api/interceptors/request/requestInterceptor.ts
+++ apps/web/src/shared/api/interceptors/request/requestInterceptor.ts
@@ -6,17 +6,32 @@ import { isClient, isServer } from '@/shared/utils';
 
 export const setRequestInterceptors = (apiClient: ApiClient) => {
   apiClient.interceptors.request.register(async (config) => {
-    let accessToken = '';
-    if (isClient) {
-      accessToken = getClientATK();
-    } else if (isServer) {
-      accessToken = await getServerATK();
-    }
+    const headers: Record<string, string> = {};
+    let nextOptions: NextFetchRequestConfig = {};
+
     // TODO: 모바일 환경분기 추가 (Capacitor SecurityStorage 접근)
 
+    if (isClient) {
+      headers.Authorization = `Bearer ${getClientATK()}`;
+    } else if (isServer) {
+      headers.Authorization = `Bearer ${await getServerATK()}`;
+      if (
+        config.options.method === 'GET' &&
+        config.options.next?.revalidate === undefined
+      ) {
+        nextOptions = { revalidate: 300 };
+      }
+    }
+
+    // 토큰 관련 헤더 추가
     config.options.headers = {
       ...config.options.headers,
-      Authorization: `Bearer ${accessToken}`,
+      ...headers,
+    };
+    // next 관련 옵션 추가
+    config.options.next = {
+      ...config.options.next,
+      ...nextOptions,
     };
 
     return config;
