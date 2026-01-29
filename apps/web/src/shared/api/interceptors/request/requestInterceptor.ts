a6b4113ad7f6b590fc5c008ff6145d8fa092484a
kyh0726 <younghyun75398@gmail.com>
2026-01-29 12:56:00 +0900

    feat: req 인터셉터 추가

--- apps/web/src/shared/api/interceptors/request/requestInterceptor.ts
+++ apps/web/src/shared/api/interceptors/request/requestInterceptor.ts
@@ -1,7 +1,24 @@
 import { ApiClient } from '@causw/api-client';
 
+import { getServerATK } from '@/shared/storage/auth';
+import { getClientATK } from '@/shared/storage/auth/auth-storage';
+import { isClient, isServer } from '@/shared/utils';
+
 export const setRequestInterceptors = (apiClient: ApiClient) => {
-  apiClient.interceptors.request.register((config) => {
+  apiClient.interceptors.request.register(async (config) => {
+    let accessToken = '';
+    if (isClient) {
+      accessToken = getClientATK();
+    } else if (isServer) {
+      accessToken = await getServerATK();
+    }
+    // TODO: 모바일 환경분기 추가 (Capacitor SecurityStorage 접근)
+
+    config.options.headers = {
+      ...config.options.headers,
+      Authorization: `Bearer ${accessToken}`,
+    };
+
     return config;
   });
 };
