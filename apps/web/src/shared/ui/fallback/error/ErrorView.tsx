fa4f0234b97f0a2aa1a37ff2d2a03e8bbb4848ac
kyh0726 <younghyun75398@gmail.com>
2026-01-31 01:07:03 +0900

    feat: empty, error, suspense fallback ui 추가

---
+++ apps/web/src/shared/ui/fallback/error/ErrorView.tsx
@@ -0,0 +1,33 @@
+import { type FallbackProps } from 'react-error-boundary';
+
+import { Button, CTAButton, Spacer, Text, VStack } from '@causw/cds';
+
+import { WarningIcon } from '../../temp';
+
+interface ErrorViewProps extends FallbackProps {
+  errorMessage?: string;
+}
+
+export function ErrorView({
+  errorMessage,
+  resetErrorBoundary,
+}: ErrorViewProps) {
+  return (
+    <VStack
+      align="center"
+      justify="center"
+      gap="none"
+      className="h-full w-full p-5"
+    >
+      <WarningIcon className="text-red-500" />
+      <Spacer size={4} />
+      <Text typography="subtitle-20-bold">
+        {errorMessage || '문제가 발생했어요'}
+      </Text>
+      <Spacer size={10} />
+      <Button onClick={resetErrorBoundary} color="red">
+        다시 시도
+      </Button>
+    </VStack>
+  );
+}
