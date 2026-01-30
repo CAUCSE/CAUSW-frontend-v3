79a55985eb8cb8f369c4c5f230d7c6df337352ff
kyh0726 <younghyun75398@gmail.com>
2026-01-30 13:49:50 +0900

    feat: refreshQueue 처리 관련 필드, 메서드 추가

--- apps/web/src/shared/api/instances/instances.ts
+++ apps/web/src/shared/api/instances/instances.ts
@@ -24,18 +24,42 @@ export class BaseApiClient {
   private client: ApiClient;
   private isRefreshing: boolean = false;
 
-  constructor(config: ApiClientConfig) {
-    this.client = createApiClient(config);
-  }
-
+  /** 원본 API Client 접근 로직 ( response wrapping 되지 않는, 토큰 관련 처리 없는 ) */
   public get internalClient() {
     return this.client;
+  }
+
+  /** Refresh Token 재발급을 위한 큐 */
+  private refreshQueue: {
+    resolve: (token: string) => void;
+    reject: () => void;
+  }[] = [];
+
+  constructor(config: ApiClientConfig) {
+    this.client = createApiClient(config);
   }
 
   public getIsRefreshing = () => this.isRefreshing;
   public setIsRefreshing = (value: boolean) => {
     this.isRefreshing = value;
   };
+
+  public addToRefreshQueue(callback: {
+    resolve: (token: string) => void;
+    reject: () => void;
+  }) {
+    this.refreshQueue.push(callback);
+  }
+
+  public processRefreshQueue(token: string) {
+    this.refreshQueue.forEach(({ resolve }) => resolve(token));
+    this.refreshQueue = [];
+  }
+
+  public rejectRefreshQueue() {
+    this.refreshQueue.forEach(({ reject }) => reject());
+    this.refreshQueue = [];
+  }
 
   public get = async <T>(
     url: string,
@@ -101,5 +125,5 @@ export class BaseApiClient {
 
 export const API = new BaseApiClient(baseConfig);
 
-setRequestInterceptors(API.internalClient);
-setResponseInterceptors(API.internalClient);
+setRequestInterceptors(API);
+setResponseInterceptors(API);
