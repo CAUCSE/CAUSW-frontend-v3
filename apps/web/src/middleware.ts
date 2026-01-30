372c2834aac5e4f9b767bed60984f9f45f06324a
kyh0726 <younghyun75398@gmail.com>
2026-01-30 17:48:42 +0900

    feat: route 관련 로직 변경

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -5,11 +5,13 @@ import { TokenManager } from './shared/storage/auth';
 
 export async function middleware(request: NextRequest) {
   // TODO: 실제 경로 맞춰서 세분화
-  const privateRoutes = ['/home/*', '/board/*', '/contacts/*', '/user/*'];
+  const privateRoutes = ['/home', '/board', '/contacts', '/user'];
   const accessToken = await TokenManager.getAccessToken();
 
   if (!accessToken) {
-    if (privateRoutes.includes(request.nextUrl.pathname)) {
+    if (
+      privateRoutes.some((route) => request.nextUrl.pathname.startsWith(route))
+    ) {
       return NextResponse.redirect(new URL('/auth/sign-in', request.url));
     }
     return NextResponse.next();
