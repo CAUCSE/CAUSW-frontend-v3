718126ffad48fe697f0bdc6408f927720a99b3a3
kyh0726 <younghyun75398@gmail.com>
2026-01-31 11:48:00 +0900

    feat: unused var 제거

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,19 +1,7 @@
 import './globals.css';
+
 import type { Metadata } from 'next';
-
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import { QueryProviderWithDevtools } from '@/shared/ui';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: '동문 네트워크',
