d7a2eaef8e1d4ed2c74e84d75b6bc3553984e446
kyh0726 <younghyun75398@gmail.com>
2026-02-02 00:02:06 +0900

    feat: suspense 컴포넌트 추가

--- apps/web/src/shared/ui/fallback/suspense/SuspenseView.tsx
+++ apps/web/src/shared/ui/fallback/suspense/SuspenseView.tsx
@@ -1,1 +1,23 @@
 // 로딩 컴포넌트 위치
+
+import { VStack } from "@causw/cds";
+
+
+import { Spinner } from "./temp/Spinner";
+
+interface SuspenseViewProps {
+  size?: number;
+}
+
+export function SuspenseView({ size = 30}: SuspenseViewProps) {
+  return (
+    <VStack
+      align="center"
+      justify="center"
+      gap="none"
+      className="h-full w-full p-5"
+    >
+      <Spinner size={size} />
+    </VStack>
+  );
+}
