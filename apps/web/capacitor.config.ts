728bf30b40bbc18514e3386dc33aa4fe1f56c747
causyj <jainayj804998@gmail.com>
2026-02-02 23:54:29 +0900

    feat: add capacitor

---
+++ apps/web/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'kr.co.causw',
+  appName: '크자회(CCSSAA)',
+  webDir: 'public',
+};
+
+export default config;
