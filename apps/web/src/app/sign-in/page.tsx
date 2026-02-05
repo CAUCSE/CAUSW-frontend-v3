057291cdc9d4afc040bdcb7b3ee88887071c06fe
kyh0726 <younghyun75398@gmail.com>
2026-02-05 12:03:20 +0900

    feat: 앱 라우터에 sign-up, sign-in 연결

---
+++ apps/web/src/app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { SignInPage } from '@/_pages/auth';
+
+const Page = () => {
+  return <SignInPage />;
+};
+
+export default Page;
---
+++ apps/web/src/app/sign-up/page.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { SignUpFunnel } from '@/_pages/auth/sign-up/funnel/SignUpFunnel';
+
+const Page = () => {
+  return <SignUpFunnel />;
+};
+
+export default Page;
