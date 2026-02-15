99ff87f17b18cbc355eec82511a723844d02a24b
kyh0726 <younghyun75398@gmail.com>
2026-02-15 17:27:32 +0900

    style: auth 컨테이너 반응형에 맞춰 수정

--- apps/web/src/widgets/auth/ui/AuthContainer/AuthContainer.tsx
+++ apps/web/src/widgets/auth/ui/AuthContainer/AuthContainer.tsx
@@ -11,7 +11,7 @@ export const AuthContainer = ({ children }: AuthContainerProps) => {
         <Flex
           gap="none"
           justify="start"
-          className="min-h-screen w-[600px] flex-col px-4 py-20"
+          className="min-h-screen w-full max-w-[37.5rem] flex-col px-5 py-4 md:px-8 md:py-20"
         >
           {children}
         </Flex>
