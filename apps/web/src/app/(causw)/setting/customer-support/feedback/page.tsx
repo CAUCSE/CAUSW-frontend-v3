099df2d0868c8eacbecc8c8b2601dd044d4cd67a
Rael <emilyekos@gmail.com>
2026-02-19 04:02:20 +0900

    revert "refactor: 코드 import 순서 정렬 #58"

--- apps/web/src/app/(causw)/setting/customer-support/feedback/page.tsx
+++
@@ -1,5 +0,0 @@
-﻿import { FeedbackReportPage } from '@/_pages/setting/customer-support';
-
-export default function Page() {
-  return <FeedbackReportPage />;
-}
--- apps/web/src/app/(causw)/setting/customer-support/terms/page.tsx
+++
@@ -1,5 +0,0 @@
-﻿import { TermsOfServicePage } from '@/_pages/setting/customer-support';
-
-export default function Page() {
-  return <TermsOfServicePage />;
-}
