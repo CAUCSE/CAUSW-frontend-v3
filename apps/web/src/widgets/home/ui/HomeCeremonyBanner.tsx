f92e10acfc9fd88747453f9a8e58bb45694ba6fe
causyj <jainayj804998@gmail.com>
2026-02-19 16:03:29 +0900

    feat: add ceremony register banner on home

---
+++ apps/web/src/widgets/home/ui/HomeCeremonyBanner.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { HStack, Text, VStack, ArrowDown } from '@causw/cds';
+
+import EnvelopeIcon from '../assets/envelope.svg';
+
+import { COPY } from '@/shared';
+
+//TODO : 경조사 신청 페이지 수정 with ROUTES
+//TODO : ArrowDown 아이콘 -> ArrowRight 아이콘으로 변경
+//TODO : ArrowRight 아이콘 색상 디자인시스템(#ffefef) 변경되면 흰색인지 확인
+export function HomeCeremonyBanner() {
+  return (
+    <Link href="/ceremony/apply" className="w-full">
+      <div className="flex w-full items-center justify-between rounded-[1rem] bg-linear-to-r from-[#3786FF] to-[#98CDFF] py-[1.125rem] pr-3 pl-5">
+        <VStack className="items-start gap-1">
+          <Text typography="subtitle-18-bold" textColor="white">
+            {COPY.CEREMONY_REGISTER}
+          </Text>
+          <Text
+            typography="body-14-medium"
+            textColor="white"
+            className="whitespace-pre-wrap opacity-70"
+          >
+            {COPY.CEREMONY_REGISTER_DESCRIPTION}
+          </Text>
+        </VStack>
+
+        <HStack className="items-center gap-5">
+          <Image src={EnvelopeIcon} alt="Envelope" width={80} height={80} />
+          <ArrowDown className="inline-block rotate-270" color="white" />
+        </HStack>
+      </div>
+    </Link>
+  );
+}
