0c3cf888cd6298d712d0211ed3a8c716c0132e7f
Sowon Kim <sso626262@naver.com>
2026-02-20 02:49:50 +0900

    feat: 신고 제출 후 토스트 추가

--- apps/web/src/features/post/model/hooks/useReportPost.ts
+++ apps/web/src/features/post/model/hooks/useReportPost.ts
@@ -1,9 +1,13 @@
+import { toast } from '@causw/cds';
+
 import { ReportReason } from '@/features';
 
 export const useReportPost = (postId: string | number) => {
+  // TODO: react query 사용
+
   const submitReport = async (reason: ReportReason) => {
     console.log('Post 신고 API 호출', postId, reason);
-    // TODO: await api.reportPost(postId, reason);
+    toast.success('신고가 접수되었어요');
   };
 
   return { submitReport };
