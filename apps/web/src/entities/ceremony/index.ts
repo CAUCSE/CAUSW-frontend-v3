c35639aaa0ff14a6a631082dd0aa0ea209da53cf
MinSeokKim <kimjunseok23@gmail.com>
2026-02-22 09:45:30 +0900

    feat: 경조사 관련 entities index 파일 정리

---
+++ apps/web/src/entities/ceremony/index.ts
@@ -0,0 +1,2 @@
+// @/entities/ceremony
+// 경조사 관련 엔티티
---
+++ apps/web/src/entities/ceremony/ui/index.ts
@@ -0,0 +1,3 @@
+export { CeremonyListItem } from './CeremonyListItem';
+export { CeremonyEmptyView } from './CeremonyEmptyView';
+export { CeremonyFilterChips } from './CeremonyFilterChips';
