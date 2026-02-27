0984c7271645630726a5f08af912cc8f695dd1a1
kyh0726 <younghyun75398@gmail.com>
2026-02-28 01:35:03 +0900

    feat: passwordInput 추상화

---
+++ apps/web/src/shared/ui/form/RHFPasswordInput.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useFormContext } from 'react-hook-form';
+
+import { Eye, EyeOff, Field, TextInput } from '@causw/cds';
+
+export interface RHFPasswordInputProps extends React.ComponentProps<
+  typeof TextInput
+> {
+  name: string;
+  label: string;
+}
+
+export const RHFPasswordInput = ({
+  name,
+  label,
+  ...props
+}: RHFPasswordInputProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const errorMessage = errors[name]?.message as string | undefined;
+
+  return (
+    <Field className="flex flex-col gap-2" error={!!errorMessage}>
+      <Field.Label>{label}</Field.Label>
+      <TextInput
+        {...props}
+        type={isVisible ? 'text' : 'password'}
+        rightIcon={
+          isVisible ? (
+            <Eye onClick={() => setIsVisible((value) => !value)} />
+          ) : (
+            <EyeOff onClick={() => setIsVisible((value) => !value)} />
+          )
+        }
+        {...register(
+          name,
+          props.onChange ? { onChange: props.onChange } : undefined,
+        )}
+      />
+      <Field.ErrorDescription>{errorMessage}</Field.ErrorDescription>
+    </Field>
+  );
+};
