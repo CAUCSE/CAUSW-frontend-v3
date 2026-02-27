25b6f55949595388a7d4c30741dfcb04ab8d78d7
kyh0726 <younghyun75398@gmail.com>
2026-02-28 00:39:16 +0900

    feat: 닉네임 변경 다이얼로그 추가

--- apps/web/src/widgets/setting/ui/index.ts
+++ apps/web/src/widgets/setting/ui/index.ts
@@ -2,3 +2,4 @@ export * from './MyActivityFeed';
 export * from './setting-overview';
 export * from './setting-notifications';
 export * from './setting-privacy';
+export * from './nickname-change-dialog';
