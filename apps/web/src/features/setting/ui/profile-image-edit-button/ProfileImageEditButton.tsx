4b2ff41cba79e72624597e22abe97b5f112f4f74
kyh0726 <younghyun75398@gmail.com>
2026-02-27 19:10:42 +0900

    feat: section에서도 역할에 맞게 features, entities로 분리

---
+++ apps/web/src/features/setting/ui/profile-image-edit-button/ProfileImageEditButton.tsx
@@ -0,0 +1,26 @@
+import { Avatar, Float, Pen } from '@causw/cds';
+
+import { PROFILE_EDIT_HREF } from '@/entities/setting';
+
+type ProfileImageEditButtonProps = {
+  onNavigate: (href: string) => void;
+};
+
+export const ProfileImageEditButton = ({
+  onNavigate,
+}: ProfileImageEditButtonProps) => {
+  return (
+    <button
+      type="button"
+      className="relative cursor-pointer rounded-full transition-opacity hover:opacity-90 active:opacity-80"
+      onClick={() => onNavigate(PROFILE_EDIT_HREF)}
+    >
+      <Avatar size="120" className="my-1 shrink-0" />
+      <Float floatType="absolute" right={0} bottom={0}>
+        <div className="flex h-6 w-6 items-center justify-center rounded-full bg-black p-1.5">
+          <Pen size={12} color="white" />
+        </div>
+      </Float>
+    </button>
+  );
+};
