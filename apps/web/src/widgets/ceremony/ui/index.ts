0ba07b790c66ecf6b836f787751ed033264deb8d
MinSeok1_2 <kimjunseok23@gmail.com>
2026-03-01 13:41:02 +0900

    feat: 내 경조사 화면 구성

--- apps/web/src/widgets/ceremony/ui/index.ts
+++ apps/web/src/widgets/ceremony/ui/index.ts
@@ -1,4 +1,5 @@
 export { CeremonySection } from './ceremony-section';
 export { CeremonyListView } from './ceremony-list-view';
+export { MyCeremonyListView } from './my-ceremony-list-view';
 export * from './CeremonyRegisterBanner';
 export * from './CeremonyListPreview';
