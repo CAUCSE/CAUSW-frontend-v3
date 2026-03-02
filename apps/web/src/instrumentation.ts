565b05af14c256247066c2ea21fae5c364adfa72
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-03 02:15:47 +0900

    feat: server 환경 전용 msw worker 및 instrumentation 파일에 적용

--- apps/web/src/instrumentation.ts
+++ apps/web/src/instrumentation.ts
@@ -3,6 +3,11 @@ import { captureRequestError } from '@causw/logger';
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     await import('../sentry.server.config');
+    if (process.env.NODE_ENV === 'development') {
+      const { server } = await import('./app/_mock/server');
+      server.listen();
+      console.log('Server Mock Started');
+    }
   }
 
   if (process.env.NEXT_RUNTIME === 'edge') {
