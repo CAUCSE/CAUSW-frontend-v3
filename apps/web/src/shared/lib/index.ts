0040ff9888f2bce8edf5f6d32810055bb95fd992
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-03 02:10:54 +0900

    feat: msw 래핑 유틸 함수 추가

--- apps/web/src/shared/lib/index.ts
+++ apps/web/src/shared/lib/index.ts
@@ -3,3 +3,4 @@ export * from './capacitor';
 export * from './daum-postcode';
 export * from './image-save';
 export * from './format';
+export { mswHttp } from './msw';
---
+++ apps/web/src/shared/lib/msw/mswHttp.ts
@@ -0,0 +1,39 @@
+import {
+  http,
+  HttpHandler,
+  HttpResponseResolver,
+  PathParams,
+  type DefaultBodyType,
+} from 'msw';
+
+import { BASE_URL } from '../../storage';
+import { DefaultResponseField } from '../../types';
+
+type MswResolver<T, U extends DefaultBodyType> = HttpResponseResolver<
+  PathParams,
+  U,
+  DefaultResponseField<T>
+>;
+
+export const mswHttp = {
+  get: <T, RequestBodyType extends DefaultBodyType = DefaultBodyType>(
+    url: string,
+    resolver: MswResolver<T, RequestBodyType>,
+  ): HttpHandler => http.get(`${BASE_URL}${url}`, resolver),
+  post: <T, RequestBodyType extends DefaultBodyType = DefaultBodyType>(
+    url: string,
+    resolver: MswResolver<T, RequestBodyType>,
+  ): HttpHandler => http.post(`${BASE_URL}${url}`, resolver),
+  put: <T, RequestBodyType extends DefaultBodyType = DefaultBodyType>(
+    url: string,
+    resolver: MswResolver<T, RequestBodyType>,
+  ): HttpHandler => http.put(`${BASE_URL}${url}`, resolver),
+  delete: <T, RequestBodyType extends DefaultBodyType = DefaultBodyType>(
+    url: string,
+    resolver: MswResolver<T, RequestBodyType>,
+  ): HttpHandler => http.delete(`${BASE_URL}${url}`, resolver),
+  patch: <T, RequestBodyType extends DefaultBodyType = DefaultBodyType>(
+    url: string,
+    resolver: MswResolver<T, RequestBodyType>,
+  ): HttpHandler => http.patch(`${BASE_URL}${url}`, resolver),
+};
