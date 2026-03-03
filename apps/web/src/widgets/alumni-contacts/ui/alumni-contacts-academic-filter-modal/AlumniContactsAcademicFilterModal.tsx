41d2bc033ba373d3a5611677492461d66243da30
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-04 03:29:40 +0900

    refactor: modal 내부 상태 관리 로직 props로 주입 받기 및 trigger 제거

--- apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-academic-filter-modal/AlumniContactsAcademicFilterModal.tsx
+++ apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-academic-filter-modal/AlumniContactsAcademicFilterModal.tsx
@@ -1,39 +1,37 @@
-'use client';
-import { useState } from 'react';
+import { Modal } from '@causw/cds';
 
-import { Modal, Text, VStack } from '@causw/cds';
+import { AlumniContactsAcademicFilterSheetModalMain } from '@/widgets/alumni-contacts/ui/alumni-contacts-academic-filter-sheet-modal-main';
 
-import { AlumniContactsAcademicFilterModalTrigger } from '@/features/alumni-contacts';
+interface AlumniContactsAcademicFilterModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  onApply: () => void;
+}
 
-export const AlumniContactsAcademicFilterModal = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleOpenChange = (open: boolean) => {
-    setIsOpen(open);
-  };
-
-  const handleClickTrigger = () => {
-    setIsOpen(true);
-  };
-
+export const AlumniContactsAcademicFilterModal = ({
+  open,
+  onOpenChange,
+  onApply,
+}: AlumniContactsAcademicFilterModalProps) => {
   return (
-    <>
-      <AlumniContactsAcademicFilterModalTrigger onClick={handleClickTrigger} />
-      <Modal open={isOpen} onOpenChange={handleOpenChange}>
-        <Modal.Title className="sr-only">
-          동문 수첩 학번, 재학 상태 필터 선택 모달창
-        </Modal.Title>
-        <Modal.Content className="items-start p-6">
-          <VStack>
-            <VStack className="gap-3">
-              <Text typography="subtitle-16-bold">학번</Text>
-            </VStack>
-            <VStack className="gap-3">
-              <Text typography="subtitle-16-bold">학적 상태</Text>
-            </VStack>
-          </VStack>
-        </Modal.Content>
-      </Modal>
-    </>
+    <Modal open={open} onOpenChange={onOpenChange}>
+      <Modal.Title className="sr-only">
+        동문 수첩 학번, 재학 상태 필터 선택 모달창
+      </Modal.Title>
+      <Modal.Content className="items-start p-6">
+        <AlumniContactsAcademicFilterSheetModalMain />
+        <Modal.Footer className="p-0">
+          <Modal.Close asChild>
+            <Modal.ActionButton
+              color="dark"
+              className="typo-body-15-semibold"
+              onClick={onApply}
+            >
+              적용하기
+            </Modal.ActionButton>
+          </Modal.Close>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
   );
 };
