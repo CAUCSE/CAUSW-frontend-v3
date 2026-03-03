41d33576cc431093cac32395662991b2f206aeea
causyj <jainayj804998@gmail.com>
2026-03-03 23:48:58 +0900

    feat: comment out unnecesary type

--- apps/web/src/entities/calendar/model/types/types.ts
+++ apps/web/src/entities/calendar/model/types/types.ts
@@ -17,22 +17,22 @@ export interface CalendarScheduleParams {
   types?: string[];
 }
 
-//TODO : HOLIDAY는 백쪽 수정되면 삭제하기
+//TODO : HOLIDAY는 기획/COMPETITION는백쪽 수정되면 삭제하기
 export type CalendarScheduleType =
   | 'ACADEMIC'
   | 'DEPARTMENT'
   | 'CCSSAA'
   | 'STUDENT_COUNCIL'
-  | 'COMPETITION'
+  // | 'COMPETITION'
   | 'HOLIDAY';
 
-//TODO : HOLIDAY는 백쪽 수정되면 삭제하기
+//TODO : HOLIDAY는 기획/COMPETITION는백쪽 수정되면 삭제하기
 export const CALENDAR_EVENTS_TYPE_MAP: Record<CalendarScheduleType, string> = {
   ACADEMIC: '학사',
   DEPARTMENT: '학부',
   CCSSAA: '크자회',
   STUDENT_COUNCIL: '집행부',
-  COMPETITION: '대회/공모전',
+  // COMPETITION: '대회/공모전',
   HOLIDAY: '공휴일',
 };
 export type CalendarEventype = 'holiday' | 'event' | 'important';
