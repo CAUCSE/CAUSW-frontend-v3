6bb9efc6cdae16e24e93b1f03467dda8ed488cbd
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-04 03:24:13 +0900

    feat: 동문 수첩 학적 상태 관련 필터 상수 및 타입 추가

--- apps/web/src/entities/alumni-contacts/config/index.ts
+++ apps/web/src/entities/alumni-contacts/config/index.ts
@@ -2,3 +2,7 @@ export {
   ALUMNI_CONTACTS_SORT_FILTER_OPTION,
   type AlumniContactsSortFilterOption,
 } from './alumniContactsSortFilterOption';
+export {
+  ALUMNI_CONTACTS_ACADEMIC_STATUS_FILTER_OPTION,
+  type AlumniContactsAcademicStatusFilterOption,
+} from './alumniContactsAcademicStatusFilterOption';
--- apps/web/src/entities/alumni-contacts/index.ts
+++ apps/web/src/entities/alumni-contacts/index.ts
@@ -1,4 +1,6 @@
 export {
   ALUMNI_CONTACTS_SORT_FILTER_OPTION,
   type AlumniContactsSortFilterOption,
+  ALUMNI_CONTACTS_ACADEMIC_STATUS_FILTER_OPTION,
+  type AlumniContactsAcademicStatusFilterOption,
 } from './config';
