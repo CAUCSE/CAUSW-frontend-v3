91e0e8780b48860bdf2d782df4a775d22a8af9ff
kyh0726 <younghyun75398@gmail.com>
2026-03-10 11:30:42 +0900

    feat: 이메일 검증 mutations 추가

--- apps/web/src/features/auth/model/mutations/index.ts
+++ apps/web/src/features/auth/model/mutations/index.ts
@@ -2,3 +2,5 @@ export * from './useSessionMutations';
 export * from './useKakaoLoginMutation';
 export * from './useAppleLoginMutation';
 export * from './useGoogleLoginMutation';
+export * from './useSendEmailVerificationCodeMutation';
+export * from './useVerifyEmailVerificationCodeMutation';
