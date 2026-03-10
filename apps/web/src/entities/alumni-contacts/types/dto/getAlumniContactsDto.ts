3637bb41209360c596d9c6090529bed7c9e9a429
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-11 00:38:35 +0900

    feat: 동문수첩 리스트 api query, dto 타입 추가

---
+++ apps/web/src/entities/alumni-contacts/types/dto/getAlumniContactsDto.ts
@@ -0,0 +1,14 @@
+import { PaginationDto } from '@/shared/types';
+
+interface GetAlumniContactsResponseDto {
+  id: string;
+  profileImageUrl: string;
+  name: string;
+  admissionYear: string;
+  academicStatus: string;
+  hob: string;
+  description: string;
+}
+
+export type GetPaginatedAlumniContactsResponseDto =
+  PaginationDto<GetAlumniContactsResponseDto>;
