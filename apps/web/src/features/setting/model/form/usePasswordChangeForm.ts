9a6b810d4c1629bf855bb74566efb3809a8bfca5
kyh0726 <younghyun75398@gmail.com>
2026-03-10 23:40:58 +0900

    feat: 닉네임, 비밀번호 폼 관련 훅 정리

---
+++ apps/web/src/features/setting/model/form/usePasswordChangeForm.ts
@@ -0,0 +1,39 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import {
+  passwordChangeFormSchema,
+  type PasswordChangeFormData,
+} from '@/entities/setting';
+
+import { useChangeMyPasswordMutation } from '../mutations';
+
+export const usePasswordChangeForm = () => {
+  const methods = useForm<PasswordChangeFormData>({
+    mode: 'onChange',
+    resolver: zodResolver(passwordChangeFormSchema),
+    defaultValues: {
+      currentPassword: '',
+      nextPassword: '',
+      confirmPassword: '',
+    },
+  });
+  const changePasswordMutation = useChangeMyPasswordMutation();
+
+  const onSubmit = (data: PasswordChangeFormData) => {
+    changePasswordMutation.mutate({
+      currentPassword: data.currentPassword,
+      newPassword: data.nextPassword,
+      newPasswordConfirm: data.confirmPassword,
+    });
+  };
+
+  return {
+    methods,
+    onSubmit,
+    isSubmitting: changePasswordMutation.isPending,
+  };
+};
