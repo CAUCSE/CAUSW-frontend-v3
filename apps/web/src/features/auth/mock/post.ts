655810523562aa82923d5bf1239af87a995af433
kyh0726 <younghyun75398@gmail.com>
2026-03-10 22:17:02 +0900

    feat: api mocking 주석 처리 대신 passThrough 활용으로 변경

--- apps/web/src/features/auth/mock/post.ts
+++ apps/web/src/features/auth/mock/post.ts
@@ -1,55 +1,58 @@
-// import { HttpResponse } from 'msw';
+import { HttpResponse, passthrough } from 'msw';
 
-// import {
-//   SigninResponseDto,
-//   SignoutResponseDto,
-//   SignupResponseDto,
-// } from '@/entities/auth';
+import {
+  SigninResponseDto,
+  SignoutResponseDto,
+  SignupResponseDto,
+} from '@/entities/auth';
 
-// import { mswHttp } from '@/shared/lib';
+import { mswHttp } from '@/shared/lib';
 
-// const AUTH_API_PREFIX = '/api/v2/auth';
+const AUTH_API_PREFIX = '/api/v2/auth';
 
 export const postHandler = [
-  // mswHttp.post<SignupResponseDto>(`${AUTH_API_PREFIX}/signup`, () => {
-  //   return HttpResponse.json(
-  //     {
-  //       code: '201',
-  //       message: 'Signup successful',
-  //       data: {
-  //         accessToken: '1234567890',
-  //         name: 'John Doe',
-  //         email: 'john.doe@example.com',
-  //         profileImgUrl: 'https://picsum.photos/seed/profile1/200/200',
-  //       },
-  //     },
-  //     {
-  //       status: 201,
-  //     },
-  //   );
-  // }),
-  // mswHttp.post<SigninResponseDto>(`${AUTH_API_PREFIX}/login`, () => {
-  //   return HttpResponse.json(
-  //     {
-  //       code: '201',
-  //       message: 'Login successful',
-  //       data: {
-  //         accessToken: '1234567890',
-  //         name: 'John Doe',
-  //         email: 'john.doe@example.com',
-  //         profileImgUrl: 'https://picsum.photos/seed/profile1/200/200',
-  //       },
-  //     },
-  //     { status: 201 },
-  //   );
-  // }),
-  // mswHttp.post<SignoutResponseDto>(`${AUTH_API_PREFIX}/logout`, () => {
-  //   return HttpResponse.json(
-  //     {
-  //       code: '204',
-  //       data: undefined,
-  //     },
-  //     { status: 204 },
-  //   );
-  // }),
+  mswHttp.post<SignupResponseDto>(`${AUTH_API_PREFIX}/signup`, () => {
+    return passthrough();
+    return HttpResponse.json(
+      {
+        code: '201',
+        message: 'Signup successful',
+        data: {
+          accessToken: '1234567890',
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+          profileImgUrl: 'https://picsum.photos/seed/profile1/200/200',
+        },
+      },
+      {
+        status: 201,
+      },
+    );
+  }),
+  mswHttp.post<SigninResponseDto>(`${AUTH_API_PREFIX}/login`, () => {
+    return passthrough();
+    return HttpResponse.json(
+      {
+        code: '201',
+        message: 'Login successful',
+        data: {
+          accessToken: '1234567890',
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+          profileImgUrl: 'https://picsum.photos/seed/profile1/200/200',
+        },
+      },
+      { status: 201 },
+    );
+  }),
+  mswHttp.post<SignoutResponseDto>(`${AUTH_API_PREFIX}/logout`, () => {
+    return passthrough();
+    return HttpResponse.json(
+      {
+        code: '204',
+        data: undefined,
+      },
+      { status: 204 },
+    );
+  }),
 ];
