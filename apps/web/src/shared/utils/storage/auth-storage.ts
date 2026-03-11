8b443ec95b710346c879520acf9d167a0633c9d0
kyh0726 <younghyun75398@gmail.com>
2026-03-11 19:29:21 +0900

    feat: 리프레시 갱신 여부에 대한 상수 추가

--- apps/web/src/shared/utils/storage/auth-storage.ts
+++ apps/web/src/shared/utils/storage/auth-storage.ts
@@ -5,5 +5,13 @@ export const getStorageAccessKey = () => {
 export const getStorageRefreshKey = () => {
   return 'refresh_token';
 };
+
+export const getStorageAuthRefreshedKey = () => {
+  return 'auth_refreshed';
+};
+
+export const REFRRESHED = '1';
+export const NOT_REFRESHED = '0';
+
 export const getNativeAccessKey = () => 'access_token';
 export const getNativeRefreshKey = () => 'refresh_token';
