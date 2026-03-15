58bef654b45671bd67bad77da1b76faf7fe57c86
Sowon Kim <sso626262@naver.com>
2026-03-15 15:15:34 +0900

    chore: 게시판 mock 데이터 수정

--- apps/web/src/entities/feed/api/mock/get.ts
+++ apps/web/src/entities/feed/api/mock/get.ts
@@ -21,7 +21,7 @@ export const getHandler = [
                 name: '일반',
               },
               {
-                id: '3c9f86a98e1861d8018e186a8acd0000',
+                id: 'a5535d2f-a991-4246-a83e-5b6b6f6974e7',
                 name: '학생회',
               },
               {
