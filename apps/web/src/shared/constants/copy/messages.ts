91fe6f7c705dc10ff051a7da7933bb53b4705bf3
causyj <jainayj804998@gmail.com>
2026-03-20 00:18:40 +0900

    fix: edit duplicated error msg

--- apps/web/src/shared/constants/copy/messages.ts
+++ apps/web/src/shared/constants/copy/messages.ts
@@ -4,6 +4,6 @@ export const MESSAGE = Object.freeze({
     PERMISSION_DENIED: '알림 권한이 허용되지 않았습니다.',
     REGISTER_ERROR: '알림 토큰 등록에 실패했습니다.',
     UNKNOWN_ERROR: '알림 설정 중 오류가 발생했습니다.',
-    TOKEN_UPDATE_FAILURE: '알림 토큰 등록에 실패했습니다.',
+    TOKEN_UPDATE_FAILURE: '서버에 알림 정보를 등록하는데 실패했습니다.',
   },
 } as const);
