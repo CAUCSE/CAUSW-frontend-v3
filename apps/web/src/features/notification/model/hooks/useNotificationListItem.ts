8707d0d94af6d3f97807e2c0ac154b2e0db446fc
lee0jae330 <xingxing2001@cau.ac.kr>
2026-03-25 01:02:58 +0900

    feat: 알림 클릭 및 읽음 처리 관련 커스텀 훅 추가

---
+++ apps/web/src/features/notification/model/hooks/useNotificationListItem.ts
@@ -0,0 +1,40 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+import {
+  type GetNotificationsResponseDto,
+  NOTIFICATION_TYPE,
+} from '@/entities/notification';
+
+import { usePatchNotificationReadStatus } from '../mutations';
+
+export const useNotificationListItem = () => {
+  const router = useRouter();
+  const { changeNotificationReadStatus } = usePatchNotificationReadStatus();
+
+  const handleClickNotification = (
+    notification: GetNotificationsResponseDto,
+  ) => {
+    if (!notification.isRead) {
+      changeNotificationReadStatus(notification.notificationLogId);
+    }
+
+    if (
+      notification.noticeType === NOTIFICATION_TYPE.COMMUNITY.type ||
+      notification.noticeType === NOTIFICATION_TYPE.OFFICIAL.type
+    ) {
+      router.push(`/feed/${notification.targetId}`);
+      return;
+    }
+
+    if (notification.noticeType === NOTIFICATION_TYPE.CEREMONY.type) {
+      router.push(`/ceremony/${notification.targetId}`);
+      return;
+    }
+  };
+
+  return {
+    handleClickNotification,
+  };
+};
