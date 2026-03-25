5d311f96484a00caa927e3cdc474a97eab9c9e09
kyh0726 <younghyun75398@gmail.com>
2026-03-26 02:12:28 +0900

    style: 제출하기 버튼 파랑으로 변경

--- apps/web/src/widgets/auth/ui/enrollment-verification-action-header/EnrollmentVerificationActionHeader.tsx
+++ apps/web/src/widgets/auth/ui/enrollment-verification-action-header/EnrollmentVerificationActionHeader.tsx
@@ -14,7 +14,11 @@ export const EnrollmentVerificationActionHeader = ({
       <ActionHeader.BackButton type="button" onClick={onCancel}>
         뒤로
       </ActionHeader.BackButton>
-      <ActionHeader.ActionButton type="submit" disabled={isSubmitDisabled}>
+      <ActionHeader.ActionButton
+        type="submit"
+        buttonColor="blue"
+        disabled={isSubmitDisabled}
+      >
         제출하기
       </ActionHeader.ActionButton>
     </ActionHeader>
