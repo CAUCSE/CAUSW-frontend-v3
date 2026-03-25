66ad4f3651ca346f649325ea8fc8545cf2d28df6
kyh0726 <younghyun75398@gmail.com>
2026-03-25 19:26:41 +0900

    feat: 학과 선택 enum 서버 데이터에 맞춰서 변경

--- apps/web/src/entities/auth/config/constants/index.ts
+++ apps/web/src/entities/auth/config/constants/index.ts
@@ -1,2 +1,6 @@
 export { ENROLLMENT_STEP_CONFIG } from './enrollment-step';
+export {
+  ENROLLMENT_VERIFICATION_DEPARTMENT,
+  ENROLLMENT_VERIFICATION_DEPARTMENT_OPTIONS,
+} from './enrollment-verification-department';
 export { ENROLLMENT_VERIFICATION_FORM_FIELD } from './enrollment-verification-form';
--- apps/web/src/features/auth/ui/enrollment-verification-major-field/EnrollmentVerificationMajorField.tsx
+++ apps/web/src/features/auth/ui/enrollment-verification-major-field/EnrollmentVerificationMajorField.tsx
@@ -1,6 +1,9 @@
 'use client';
 
-import { ENROLLMENT_VERIFICATION_FORM_FIELD } from '@/entities/auth';
+import {
+  ENROLLMENT_VERIFICATION_DEPARTMENT_OPTIONS,
+  ENROLLMENT_VERIFICATION_FORM_FIELD,
+} from '@/entities/auth';
 
 import { RHFTabSelect } from '@/shared/ui';
 
@@ -9,13 +12,7 @@ export const EnrollmentVerificationMajorField = () => {
     <RHFTabSelect
       name={ENROLLMENT_VERIFICATION_FORM_FIELD.major}
       label="학과(부)"
-      options={[
-        { label: '전산학과', value: '전산학과' },
-        { label: '컴퓨터공학과', value: '컴퓨터공학과' },
-        { label: '컴퓨터공학부', value: '컴퓨터공학부' },
-        { label: '소프트웨어학부', value: '소프트웨어학부' },
-        { label: 'AI학과', value: 'AI학과' },
-      ]}
+      options={ENROLLMENT_VERIFICATION_DEPARTMENT_OPTIONS}
       required
     />
   );
