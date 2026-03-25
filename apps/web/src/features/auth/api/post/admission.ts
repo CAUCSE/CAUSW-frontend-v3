9ac0fe2311d358da9973b3a3477c388ab210d74c
kyh0726 <younghyun75398@gmail.com>
2026-03-25 19:55:06 +0900

    feat: 재학증빙 서류 제출 api 호출부 정의

---
+++ apps/web/src/features/auth/api/post/admission.ts
@@ -0,0 +1,23 @@
+import { type AdmissionCreateRequestDto } from '@/entities/auth';
+
+import { API } from '@/shared/api';
+
+const URL_PREFIX = '/api/v2/users/me/admission';
+
+export const createAdmission = async ({
+  request,
+  attachImages,
+}: AdmissionCreateRequestDto) => {
+  const formData = new FormData();
+
+  formData.append(
+    'request',
+    new Blob([JSON.stringify(request)], { type: 'application/json' }),
+  );
+
+  attachImages.forEach((file) => {
+    formData.append('attachImages', file);
+  });
+
+  return API.post<void>(URL_PREFIX, formData);
+};
--- apps/web/src/features/auth/api/post/index.ts
+++ apps/web/src/features/auth/api/post/index.ts
@@ -1,1 +1,2 @@
+export * from './admission';
 export * from './session';
