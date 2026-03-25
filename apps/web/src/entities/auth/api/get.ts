3d52e7a23022750e98d3dedb705df26b1acf4c8a
kyh0726 <younghyun75398@gmail.com>
2026-03-25 22:14:02 +0900

    feat: 현재 재학 등록 상태 api 호출부 작성

--- apps/web/src/entities/auth/api/get.ts
+++ apps/web/src/entities/auth/api/get.ts
@@ -1,20 +1,29 @@
 import { API } from '@/shared/api';
 
 import {
+  type AdmissionStateResponseDto,
   type CheckNicknameDuplicateRequestDto,
   type CheckPhoneDuplicateRequestDto,
 } from '../model/types';
+
+const AUTH_API_URL_PREFIX = '/api/v2/users';
+
+export const getAdmissionState = async () => {
+  return API.get<AdmissionStateResponseDto>(
+    `${AUTH_API_URL_PREFIX}/me/admission/state`,
+  );
+};
 
 export const checkPhoneDuplicate = async (
   params: CheckPhoneDuplicateRequestDto,
 ) => {
   const query = new URLSearchParams({ phoneNumber: params.phoneNumber });
-  return API.get<null>(`/api/v2/users/check-phone?${query}`);
+  return API.get<null>(`${AUTH_API_URL_PREFIX}/check-phone?${query}`);
 };
 
 export const checkNicknameDuplicate = async (
   params: CheckNicknameDuplicateRequestDto,
 ) => {
   const query = new URLSearchParams({ nickname: params.nickname });
-  return API.get<null>(`/api/v2/users/check-nickname?${query}`);
+  return API.get<null>(`${AUTH_API_URL_PREFIX}/check-nickname?${query}`);
 };
