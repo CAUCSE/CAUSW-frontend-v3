de73a2dff6a3ec22849ebf258688a001b325d8df
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-07 02:47:38 +0900

    feat: 동문수첩 기본 프로필 및 한줄 소개 컴포넌트 추가

---
+++ apps/web/src/entities/alumni-contacts/ui/alumni-contacts-description/index.ts
@@ -0,0 +1,1 @@
+export { AlumniContactsDescription } from './AlumniContactsDescription';
--- apps/web/src/entities/alumni-contacts/ui/index.ts
+++ apps/web/src/entities/alumni-contacts/ui/index.ts
@@ -1,1 +1,3 @@
 export { AlumniContactsAcademicFilterSheetModalProvider } from './alumni-contacts-academic-filter-sheet-modal-provider';
+export { AlumniContactsBasicInfo } from './alumni-contacts-basic-info';
+export { AlumniContactsDescription } from './alumni-contacts-description';
