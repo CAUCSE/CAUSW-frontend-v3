3db4bd4286adde5b8100fe3eaeecffa7f1cf6b87
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-07 02:49:58 +0900

    feat: 동문수첩 프로필 관련 컴포넌트 추가

---
+++ apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-detail-hero/AlumniContactsDetailHero.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { VStack } from '@causw/cds';
+
+import {
+  AlumniContactsBasicInfo,
+  AlumniContactsDescription,
+  type GetAlumniContactsDetailResponseDto,
+} from '@/entities/alumni-contacts';
+
+import { AlumniContactsContactActions } from '../alumni-contacts-contact-actions';
+
+interface AlumniContactsDetailHeroProps {
+  alumniContactsDetail: GetAlumniContactsDetailResponseDto;
+}
+
+export const AlumniContactsDetailHero = ({
+  alumniContactsDetail,
+}: AlumniContactsDetailHeroProps) => {
+  return (
+    <VStack gap="lg" className="my-6 w-full px-6" as="section">
+      <VStack>
+        <AlumniContactsBasicInfo
+          name={alumniContactsDetail.name}
+          admissionYear={alumniContactsDetail.admissionYear}
+          academicStatus={alumniContactsDetail.academicStatus}
+          job={alumniContactsDetail.job}
+          profileImageUrl={alumniContactsDetail.profileImage.profileImageUrl}
+        />
+        <AlumniContactsDescription
+          description={alumniContactsDetail.description}
+        />
+      </VStack>
+      <AlumniContactsContactActions
+        isPhoneNumberVisible={alumniContactsDetail.isPhoneNumberVisible}
+        phoneNumber={alumniContactsDetail.phoneNumber}
+        email={alumniContactsDetail.email}
+      />
+    </VStack>
+  );
+};
