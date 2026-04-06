b2d4fecb8f526887d79e6f2399eb1f7b7749195f
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-07 02:43:12 +0900

    feat: 동문수첩 상세 조회 api 관련 type 정의

---
+++ apps/web/src/entities/alumni-contacts/model/types/dto/getAlumniContactsDetailDto.ts
@@ -0,0 +1,49 @@
+type ProfileImageUrl =
+  | 'MALE_1'
+  | 'MALE_2'
+  | 'FEMALE_1'
+  | 'FEMALE_2'
+  | 'CUSTOM'
+  | 'GHOST';
+
+interface ProfileImageDto {
+  profileImageType: ProfileImageUrl;
+  profileImageUrl: string;
+}
+
+interface UserCareerResponse {
+  id: string;
+  startYear: number;
+  startMonth: number;
+  endYear: number;
+  endMonth: number;
+  description: string;
+}
+
+interface UserProjectResponse {
+  id: string;
+  startYear: number;
+  startMonth: number;
+  endYear: number;
+  endMonth: number;
+  description: string;
+}
+
+export interface GetAlumniContactsDetailResponseDto {
+  id: string;
+  profileImage: ProfileImageDto;
+  name: string;
+  admissionYear: string;
+  academicStatus: string;
+  job: string;
+  description: string;
+  phoneNumber: string;
+  isPhoneNumberVisible: boolean;
+  email: string;
+  socialLinks: string[];
+  userTechStack: string[];
+  userCareer: UserCareerResponse;
+  userProject: UserProjectResponse;
+  userInterestTech: string[];
+  userInterestDomain: string[];
+}
---
+++ apps/web/src/entities/alumni-contacts/model/types/param/GetAlumniContactsDetailParam.ts
@@ -0,0 +1,3 @@
+export interface GetAlumniContactsDetailParam {
+  alumniContactsId: string;
+}
