7ba4ddb4481ca371f587d4fdae35a5934925d141
kyh0726 <younghyun75398@gmail.com>
2026-04-12 01:16:24 +0900

    feat: 로그인 이후 라우팅 함수 props 구조 변경

--- apps/web/src/features/auth/lib/routeAfterSignIn.ts
+++ apps/web/src/features/auth/lib/routeAfterSignIn.ts
@@ -1,18 +1,18 @@
 import type { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
-import type {  OnboardingStatus, UserMeResponseDto } from '@/entities/auth';
+import type { OnboardingStatus } from '@/entities/auth';
 
 export const routeAfterSignIn = (
   router: AppRouterInstance,
-  data: Pick<UserMeResponseDto, 'onboardingStatus'>,
+  onboardingStatus: OnboardingStatus,
 ) => {
   const AUTH_ROUTE_MAP: Record<OnboardingStatus, string> = {
-    'EMAIL_VERIFICATION_REQUIRED': '/auth/sign-up/email-verification',
-    'GUEST': '/auth/sign-up/oauth-additional-info',
-    'ACADEMIC_CERTIFICATION_REQUIRED': '/auth/sign-up/enrollment-verification',
-    'ACTIVE': '/home',
-    'TERMS_REQUIRED': '/home',
+    EMAIL_VERIFICATION_REQUIRED: '/auth/sign-up/email-verification',
+    GUEST: '/auth/sign-up/oauth-additional-info',
+    ACADEMIC_CERTIFICATION_REQUIRED: '/auth/sign-up/enrollment-verification',
+    ACTIVE: '/home',
+    TERMS_REQUIRED: '/home',
   } as const;
 
-  router.replace(AUTH_ROUTE_MAP[data.onboardingStatus]);
+  router.replace(AUTH_ROUTE_MAP[onboardingStatus]);
 };
