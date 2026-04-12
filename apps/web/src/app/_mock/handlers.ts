01258246ba21a43207c3f2f7e1e61202bd15efb6
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-13 04:14:05 +0900

    feat: 동문수첩 상세조회 mock 핸들러 및 mock 데이터 추가

--- apps/web/src/app/_mock/handlers.ts
+++ apps/web/src/app/_mock/handlers.ts
@@ -1,6 +1,7 @@
 // feature/auth에서 export시 ui 파일이 instrumentation.ts까지 import되는 문제가 있어 직접 import
 import { authHandler } from '@/features/auth/mock';
 
+import { alumniContactsHandler } from '@/entities/alumni-contacts';
 import { boardsHandler } from '@/entities/feed';
 import { notificationHandler } from '@/entities/notification';
 
@@ -8,4 +9,5 @@ export const handlers = [
   ...boardsHandler,
   ...notificationHandler,
   ...authHandler,
+  ...alumniContactsHandler,
 ];
