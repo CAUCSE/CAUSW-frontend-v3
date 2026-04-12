8ff8b54beaf37f6b9a915f705e7e1a224fbc2a18
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-13 04:45:57 +0900

    chore: 동문수첩 목록 페이지 import 순서 정리

--- apps/web/src/app/(causw)/alumni-contacts/page.tsx
+++ apps/web/src/app/(causw)/alumni-contacts/page.tsx
@@ -1,8 +1,8 @@
+import { AlumniContactsListPage } from '@/_pages/alumni-contacts';
+
 import { type AlumniContactsFilter } from '@/entities/alumni-contacts';
 
 import { type NextSearchParams } from '@/shared/types';
-
-import { AlumniContactsListPage } from '@/_pages/alumni-contacts';
 
 export default function Page({
   searchParams,
