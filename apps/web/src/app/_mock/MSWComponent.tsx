f83d8d5ee64cf910c803ca6ecb815df52c8271f5
causyj <jainayj804998@gmail.com>
2026-04-12 12:11:15 +0900

    feat: ignore msw on app

--- apps/web/src/app/_mock/MSWComponent.tsx
+++ apps/web/src/app/_mock/MSWComponent.tsx
@@ -2,6 +2,8 @@
 
 import { type PropsWithChildren, Suspense, use } from 'react';
 
+import { Capacitor } from '@capacitor/core';
+
 import { ENVIRONMENT } from '@/shared/config';
 
 declare global {
@@ -11,25 +13,28 @@ declare global {
     };
   }
 }
+const isBrowser = typeof window !== 'undefined';
+const isNativePlatform = isBrowser && Capacitor.isNativePlatform();
 
-const startMockWorkerPromise =
-  ENVIRONMENT === 'development' && typeof window !== 'undefined'
-    ? import('./browser').then(async ({ worker }) => {
-        await worker.start({
-          onUnhandledRequest(request, print) {
-            if (request.url.includes('_next')) {
-              return;
-            }
-            print.warning();
-          },
-        });
+const shouldStartMSW =
+  ENVIRONMENT === 'development' && isBrowser && !isNativePlatform;
+const startMockWorkerPromise = shouldStartMSW
+  ? import('./browser').then(async ({ worker }) => {
+      await worker.start({
+        onUnhandledRequest(request, print) {
+          if (request.url.includes('_next')) {
+            return;
+          }
+          print.warning();
+        },
+      });
 
-        // HMR 시 worker 중지 (handler 중복 등록 방지)
-        import.meta.hot?.dispose(() => {
-          worker.stop();
-        });
-      })
-    : Promise.resolve();
+      // HMR 시 worker 중지 (handler 중복 등록 방지)
+      import.meta.hot?.dispose(() => {
+        worker.stop();
+      });
+    })
+  : Promise.resolve();
 
 const MSWPromise = ({ children }: PropsWithChildren) => {
   use(startMockWorkerPromise);
