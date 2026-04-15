54748b5eabd3e6deed2674dce3b23d28b26c0474
kyh0726 <younghyun75398@gmail.com>
2026-04-15 20:18:21 +0900

    feat: 홈 컴포넌트 로딩상태관리 추가

--- apps/web/src/widgets/user/UserGreeting.tsx
+++ apps/web/src/widgets/user/UserGreeting.tsx
@@ -1,6 +1,15 @@
-import { Text } from '@causw/cds';
+import { Skeleton, Text } from '@causw/cds';
+
+import { useGetMeQuery } from '@/features/auth';
 
 export function UserGreeting() {
-  //TODO: api 연결 후 이름 변경
-  return <Text typography="title-32-bold">유지아님, 반갑습니다!</Text>;
+  const { data, isLoading } = useGetMeQuery();
+
+  if (isLoading) {
+    return <Skeleton width={400} height={52} />;
+  }
+
+  return (
+    <Text typography="title-32-bold">{`${data?.name}님, 반갑습니다!`}</Text>
+  );
 }
