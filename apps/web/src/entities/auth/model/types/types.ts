f7046b33c71aa457d269cf95495507b7a2becf50
kyh0726 <younghyun75398@gmail.com>
2026-04-15 20:17:49 +0900

    feat: userRes dto에 이메일 필드 추가

--- apps/web/src/entities/auth/model/types/types.ts
+++ apps/web/src/entities/auth/model/types/types.ts
@@ -41,6 +41,7 @@ export interface UserResponseDto {
   id: string;
   email: string;
   name: string;
+  email: string;
   nickname: string;
   onboardingStatus: OnboardingStatus;
   profileImage: {
