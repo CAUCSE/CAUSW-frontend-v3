6146e91cb3b6c1e93d406bffe5afe19454dde491
kyh0726 <younghyun75398@gmail.com>
2026-04-15 13:19:32 +0900

    feat: 프로필 타입, 프로필 url을 통해서 최적화된 url 반환하는 헬퍼함수 정의

---
+++ apps/web/src/shared/lib/profile-image/getProfileImageUrl.ts
@@ -0,0 +1,50 @@
+import { DEFAULT_PROFILE_IMAGE_TYPES } from '@/shared/constants';
+import type {
+  DefaultProfileImageType,
+  UserProfileImageType,
+} from '@/shared/types';
+
+import { getOptimizedImageUrl } from '../cloudfront';
+
+interface ProfileImageProps {
+  profileImageType: UserProfileImageType;
+  profileImageUrl?: string | null;
+  width?: number;
+  quality?: number;
+}
+
+const DEFAULT_PROFILE_AVATAR_PATHS = Object.fromEntries(
+  DEFAULT_PROFILE_IMAGE_TYPES.map((type) => [
+    type,
+    `/images/profile/${type}.svg`,
+  ]),
+) as Record<DefaultProfileImageType, string>;
+
+const DEFAULT_PROFILE_IMAGE_URL_MAP: Record<
+  Exclude<UserProfileImageType, 'CUSTOM'>,
+  string
+> = {
+  ...DEFAULT_PROFILE_AVATAR_PATHS,
+  GHOST: '/images/profile/MALE_1.svg',
+};
+
+export const getProfileImageUrl = ({
+  profileImageType,
+  profileImageUrl,
+  width,
+  quality,
+}: ProfileImageProps) => {
+  if (profileImageType === 'CUSTOM') {
+    if (!profileImageUrl) {
+      return profileImageUrl;
+    }
+
+    return getOptimizedImageUrl(profileImageUrl, {
+      width,
+      quality,
+      format: 'webp',
+    });
+  }
+
+  return DEFAULT_PROFILE_IMAGE_URL_MAP[profileImageType];
+};
