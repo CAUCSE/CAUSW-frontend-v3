a3ec9d0219e5a772ccb2888cd93a69b2bb145ca4
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-17 14:24:01 +0900

    feat: 동문수첩 각 카테고리별 section factory 컴포넌트 분리

--- apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-detail-info-section/AlumniContactsDetailInfoSection.tsx
+++ apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-detail-info-section/AlumniContactsDetailInfoSection.tsx
@@ -4,72 +4,14 @@ import { Text, VStack } from '@causw/cds';
 
 import {
   ALUMNI_CONTACTS_DETAIL_SECTION_TAB_LABEL,
+  type AlumniContactsDetailSectionTabType,
   type GetAlumniContactsDetailResponseDto,
 } from '@/entities/alumni-contacts';
 
-import { AlumniContactsCareerSection } from '../alumni-contacts-career-section';
-import { AlumniContactsInterestDomainSection } from '../alumni-contacts-interest-domain-section';
-import { AlumniContactsInterestTechSection } from '../alumni-contacts-interest-tech-section';
-import { AlumniContactProjectSection } from '../alumni-contacts-project-section';
-import { AlumniContactsSnsSection } from '../alumni-contacts-sns-section';
-import { AlumniContactsTechStackSection } from '../alumni-contacts-tech-stack-section';
-
-type AlumniContactDetailTabKey =
-  keyof typeof ALUMNI_CONTACTS_DETAIL_SECTION_TAB_LABEL;
-
-interface AlumniContactsDetailInfoContentProps {
-  tabType: AlumniContactDetailTabKey;
-  alumniContactsDetail: GetAlumniContactsDetailResponseDto;
-}
-
-const AlumniContactsDetailInfoContent = ({
-  tabType,
-  alumniContactsDetail,
-}: AlumniContactsDetailInfoContentProps) => {
-  switch (tabType) {
-    case 'sns':
-      return (
-        <AlumniContactsSnsSection
-          socialLinks={alumniContactsDetail.socialLinks}
-        />
-      );
-    case 'techStack':
-      return (
-        <AlumniContactsTechStackSection
-          userTechStack={alumniContactsDetail.userTechStack}
-        />
-      );
-    case 'career':
-      return (
-        <AlumniContactsCareerSection
-          userCareer={alumniContactsDetail.userCareer}
-        />
-      );
-    case 'project':
-      return (
-        <AlumniContactProjectSection
-          userProject={alumniContactsDetail.userProject}
-        />
-      );
-    case 'interestTech':
-      return (
-        <AlumniContactsInterestTechSection
-          userInterestTech={alumniContactsDetail.userInterestTech}
-        />
-      );
-    case 'interestDomain':
-      return (
-        <AlumniContactsInterestDomainSection
-          userInterestDomain={alumniContactsDetail.userInterestDomain}
-        />
-      );
-    default:
-      return null;
-  }
-};
+import { AlumniContactsDetailInfoContent } from '../alumni-contacts-detail-info-content';
 
 interface AlumniContactsDetailInfoSectionProps {
-  tabType: AlumniContactDetailTabKey;
+  tabType: AlumniContactsDetailSectionTabType;
   idx: number;
   categoryRef: RefObject<HTMLSpanElement[]>;
   alumniContactsDetail: GetAlumniContactsDetailResponseDto;
