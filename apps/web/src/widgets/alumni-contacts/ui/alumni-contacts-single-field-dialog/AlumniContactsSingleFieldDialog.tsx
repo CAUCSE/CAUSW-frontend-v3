4f7ab6c3e5afcade503c0ca3d7ebc4366bc17f05
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-17 14:26:38 +0900

    feat: 동문수첩 내용 추가 시 textinput 하나만 있는 dialog 재사용 컴포넌트 추가

---
+++ apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-single-field-dialog/AlumniContactsSingleFieldDialog.tsx
@@ -0,0 +1,58 @@
+import { type PropsWithChildren, type RefObject } from 'react';
+
+import { Button, Dialog, HStack, Text } from '@causw/cds';
+
+interface AlumniContactsSingleFieldDialogProps extends PropsWithChildren {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+  canConfirm: boolean;
+  onConfirm: () => void;
+  confirmButtonRef?: RefObject<HTMLButtonElement | null>;
+}
+
+export const AlumniContactsSingleFieldDialog = ({
+  children,
+  isOpen,
+  onOpenChange,
+  title,
+  canConfirm,
+  onConfirm,
+  confirmButtonRef,
+}: AlumniContactsSingleFieldDialogProps) => {
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <Dialog.Content className="w-80 gap-2 md:w-105">
+        <Dialog.Title>
+          <Text typography="subtitle-18-bold" textColor="gray-700">
+            {title}
+          </Text>
+        </Dialog.Title>
+        {children}
+        <Dialog.Footer>
+          <HStack gap="sm" className="pt-6">
+            <Dialog.Close asChild>
+              <Button color="gray" className="h-13 flex-1 rounded-md">
+                <Text typography="body-15-semibold" textColor="gray-600">
+                  닫기
+                </Text>
+              </Button>
+            </Dialog.Close>
+            <Dialog.Close asChild>
+              <Button
+                onClick={onConfirm}
+                className="h-13 flex-1 rounded-md bg-gray-700 text-white hover:bg-gray-800! disabled:bg-gray-200! disabled:[&_span]:text-gray-300!"
+                disabled={!canConfirm}
+                ref={confirmButtonRef}
+              >
+                <Text typography="body-15-semibold" textColor="white">
+                  추가하기
+                </Text>
+              </Button>
+            </Dialog.Close>
+          </HStack>
+        </Dialog.Footer>
+      </Dialog.Content>
+    </Dialog>
+  );
+};
