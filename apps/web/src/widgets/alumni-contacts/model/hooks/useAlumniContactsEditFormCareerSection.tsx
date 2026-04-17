2d5702292f6da29a0c8c345e4122f4866f4dedea
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-18 04:57:18 +0900

    feat: 동문수첩 수정 form 경력 section 컴포넌트 및 훅 추가

---
+++ apps/web/src/widgets/alumni-contacts/model/hooks/useAlumniContactsEditFormCareerSection.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useFieldArray, useFormContext } from 'react-hook-form';
+
+import {
+  ALUMNI_CONTACTS_EDIT_FORM_FIELD,
+  type AlumniContactsEditForm,
+} from '@/entities/alumni-contacts';
+
+import { sortAlumniContactsProfileEntry } from '../sortAlumniContactsProfileEntry';
+
+export const useAlumniContactsEditFormCareerSection = () => {
+  const { control, getValues } = useFormContext<AlumniContactsEditForm>();
+  const { fields, remove, replace } = useFieldArray({
+    control,
+    name: ALUMNI_CONTACTS_EDIT_FORM_FIELD.USER_CAREER,
+  });
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleClickAddButton = () => {
+    setIsOpen(true);
+  };
+
+  const handleOpenChange = (open: boolean) => {
+    setIsOpen(open);
+  };
+
+  const handleClickAddCareer = (
+    entry: string,
+    isCurrent: boolean,
+    startDate?: Date,
+    endDate?: Date,
+  ) => {
+    if (!startDate) {
+      return;
+    }
+
+    const currentCareer = getValues(
+      ALUMNI_CONTACTS_EDIT_FORM_FIELD.USER_CAREER,
+    );
+
+    const newCareer = {
+      description: entry,
+      startYear: startDate.getFullYear(),
+      startMonth: startDate.getMonth() + 1,
+      endYear: isCurrent ? null : (endDate?.getFullYear() ?? null),
+      endMonth: isCurrent
+        ? null
+        : endDate?.getMonth()
+          ? endDate.getMonth() + 1
+          : null,
+    };
+
+    const newCareerList = [...currentCareer, newCareer].sort(
+      sortAlumniContactsProfileEntry,
+    );
+
+    replace(newCareerList);
+    return;
+  };
+
+  const handleClickDeleteCareer = (idx: number) => {
+    remove(idx);
+  };
+
+  return {
+    fields,
+    isOpen,
+    handleClickAddButton,
+    handleOpenChange,
+    handleClickAddCareer,
+    handleClickDeleteCareer,
+  };
+};
