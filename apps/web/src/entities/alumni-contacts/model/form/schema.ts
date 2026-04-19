fbc5bd2d5f0bdc44731095547c71bade50693164
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-20 00:02:08 +0900

    refactor: 동문수첩 수정 form 스키마 관련 최대 글자수 상수로 교체

--- apps/web/src/entities/alumni-contacts/model/form/schema.ts
+++ apps/web/src/entities/alumni-contacts/model/form/schema.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+import { ALUMNI_CONTACTS_EDIT_FORM_MAX_LENGTH } from '../../config';
+
 const periodSchema = z.array(
   z.object({
     id: z.string().optional(),
@@ -7,17 +9,29 @@ const periodSchema = z.array(
     startMonth: z.number().min(1).max(12),
     endYear: z.number().min(1900).max(new Date().getFullYear()).nullable(),
     endMonth: z.number().min(1).max(12).nullable(),
-    description: z.string().min(0).max(50),
+    description: z
+      .string()
+      .min(0)
+      .max(ALUMNI_CONTACTS_EDIT_FORM_MAX_LENGTH.CAREER_DESCRIPTION),
   }),
 );
 
+const careerSchema = periodSchema;
+const projectSchema = periodSchema;
+
 export const alumniContactsEditSchema = z.object({
-  description: z.string().min(0).max(100),
+  description: z
+    .string()
+    .min(0)
+    .max(ALUMNI_CONTACTS_EDIT_FORM_MAX_LENGTH.DESCRIPTION),
   isPhoneNumberVisible: z.boolean(),
-  socialLinks: z.array(z.string()).min(0).max(10),
+  socialLinks: z
+    .array(z.string())
+    .min(0)
+    .max(ALUMNI_CONTACTS_EDIT_FORM_MAX_LENGTH.SOCIAL_LINKS),
   userTechStack: z.array(z.string()),
-  userCareer: periodSchema,
-  userProject: periodSchema,
+  userCareer: careerSchema,
+  userProject: projectSchema,
   userInterestTech: z.array(z.string()),
   userInterestDomain: z.array(z.string()),
 });
