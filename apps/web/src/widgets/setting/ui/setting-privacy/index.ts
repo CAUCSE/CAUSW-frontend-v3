e93edf4699c9941bca6be259de1daeefcad1ce10
MinSeokKim <kimjunseok23@gmail.com>
2026-04-20 17:52:25 +0900

    feat: SNS 연동 섹션 UI 추가 및 disabled 설정

--- apps/web/src/widgets/setting/ui/setting-privacy/index.ts
+++ apps/web/src/widgets/setting/ui/setting-privacy/index.ts
@@ -1,4 +1,5 @@
 export * from './PrivacyBasicInfoSection';
 export * from './PrivacyAcademicInfoSection';
 export * from './PrivacyEnrollmentStatusSection';
+export * from './PrivacySocialSection';
 export * from './PrivacyActionSection';
