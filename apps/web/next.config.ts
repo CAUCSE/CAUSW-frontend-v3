aace0f7fd5e8f585941067b73df103721cf3bb54
causyj <jainayj804998@gmail.com>
2026-04-24 22:15:00 +0900

    feat: add dev origin for android local dev

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,7 @@ import { withSentryConfig } from '@causw/logger/config';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['@causw/logger'],
+  allowedDevOrigins: ['10.0.2.2'],
 };
 
 export default withSentryConfig<NextConfig>(nextConfig, 'causw');
