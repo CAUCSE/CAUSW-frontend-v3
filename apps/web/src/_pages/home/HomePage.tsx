479f9166d53fd74ce90476ee3e6a159b094736a2
kyh0726 <younghyun75398@gmail.com>
2026-04-24 12:19:54 +0900

    feat: 홈 페이지에서 현재 유저 재학 상태에 따라 동적으로 변하는 구조로 변경

--- apps/web/src/_pages/home/HomePage.tsx
+++ apps/web/src/_pages/home/HomePage.tsx
@@ -1,68 +1,34 @@
-'use client';
-
 import { Suspense } from 'react';
 
-import { Skeleton, VStack } from '@causw/cds';
-
-import { CalendarEventListPreview } from '@/widgets/calendar';
 import {
-  CeremonyListPreview,
-  CeremonyRegisterBanner,
-} from '@/widgets/ceremony';
-import {
-  NotificationMobileHeader,
-  // NotificationPopupCard,
-} from '@/widgets/notification';
-import { UserGreetingHeader } from '@/widgets/user';
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
-import { QuickMenu } from '@/shared/ui';
+import { HomePageContent } from '@/widgets/home';
 
-//TODO : 해당 페이지 나오면 상세 페이지들 주소 수정
-//TODO : 졸업생 정보
+import { authQueryOptions } from '@/entities/auth';
 
-export function HomePage() {
-  const isAlumni = true;
+import { QUERY_STALE_TIME } from '@/shared/constants';
+import { SuspenseView } from '@/shared/ui';
+
+export async function HomePage() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: QUERY_STALE_TIME.LONG,
+      },
+    },
+  });
+
+  queryClient.prefetchQuery(authQueryOptions.me());
 
   return (
-    <VStack className="tablet:gap-8 max-w-desktop tablet:px-8 tablet:pt-12 desktop:gap-6 mx-auto w-full gap-2 px-4 pb-[2.125rem]">
-      {/* Mobile Header */}
-      <div className="tablet:hidden sticky top-0 z-10">
-        <NotificationMobileHeader />
-      </div>
-
-      {/* Desktop Greeting */}
-      <div className="tablet:block hidden">
-        <Suspense fallback={<Skeleton width={400} height={52} />}>
-          <UserGreetingHeader />
-        </Suspense>
-      </div>
-
-      <VStack className="desktop:gap-6 gap-4">
-        {/* TODO : 20260303 주석 처리 : 기획 및 api 변경으로 인해 임시 주석 -> api 수정 후에 다시 노출 (모든 알림 -> 서비스 공지로 수정) */}
-        {/* <NotificationPopupCard /> */}
-        {isAlumni && (
-          <div className="desktop:block hidden">
-            <CeremonyRegisterBanner />
-          </div>
-        )}
-
-        <div className="desktop:grid-cols-2 desktop:gap-x-6 desktop:gap-y-8 grid w-full grid-cols-1 gap-4">
-          {!isAlumni && <QuickMenu />}
-
-          {!isAlumni && (
-            <div className="desktop:block hidden">
-              <CeremonyRegisterBanner />
-            </div>
-          )}
-
-          <CalendarEventListPreview />
-
-          <div className="desktop:hidden">
-            <CeremonyRegisterBanner />
-          </div>
-          <CeremonyListPreview />
-        </div>
-      </VStack>
-    </VStack>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<SuspenseView />}>
+        <HomePageContent />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
