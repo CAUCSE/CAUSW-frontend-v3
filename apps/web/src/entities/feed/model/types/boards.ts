5db03083f58c24beb5762ae6497caa9f4656800e
lee0jae330 <xingxing2001@cau.ac.kr>
2026-04-27 17:52:49 +0900

    refactor: 게시판 관련 type 정의 구조 변경

--- apps/web/src/entities/feed/model/types/boards.ts
+++ apps/web/src/entities/feed/model/types/boards.ts
@@ -3,8 +3,3 @@ export interface Board {
   id: string;
   name: string;
 }
-
-/** 조회 가능한 게시판 리스트 */
-export interface BoardAvailableListResponseDto {
-  boards: Board[];
-}
---
+++ apps/web/src/entities/feed/model/types/dto/getAvailableBoardListDto.ts
@@ -0,0 +1,5 @@
+import { type Board } from '../boards';
+
+export interface GetAvailableBoardListResponseDto {
+  boards: Board[];
+}
