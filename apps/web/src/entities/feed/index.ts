dff248520b14c427e3daae7f543221188a9bce44
lee0jae330 <xingxing2001@cau.ac.kr>
2026-05-03 01:31:25 +0900

    feat: 게시글 검색어 및 최근 검색어 관련 상수 추가

--- apps/web/src/entities/feed/index.ts
+++ apps/web/src/entities/feed/index.ts
@@ -1,5 +1,18 @@
 export * from './api';
-export * from './config';
-export * from './model';
+export {
+  boardQueryKeys,
+  boardQueryOptions,
+  FEED_RECENT_SEARCH_KEYWORD_STORAGE_KEY,
+  FEED_RECENT_SEARCH_KEYWORD_STORAGE_INITIAL_VALUE,
+  FEED_SEARCH_KEYWORD_SEARCH_PARAM_KEY,
+} from './config';
+export {
+  useGetAvailableBoards,
+  useFeedSearchKeywordStore,
+  useSyncFeedKeywordFromSearchParam,
+  useUpdateFeedKeywordSearchParam,
+  type Board,
+  type GetAvailableBoardListResponseDto,
+} from './model';
 export * from './ui';
 export { boardsHandler } from './mock';
