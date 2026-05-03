4b05e23f3b6faf19a3e36a0d5429d4715e7a38a3
kyh0726 <younghyun75398@gmail.com>
2026-05-03 13:20:39 +0900

    feat: 데스크톱 이메일, 소셜 로그인 로직에 sessionPersist 여부 추가

--- apps/web/src/entities/auth/model/form/sign-in/schema.ts
+++ apps/web/src/entities/auth/model/form/sign-in/schema.ts
@@ -5,6 +5,7 @@ import { emailSchema, passwordSchema } from '@/shared/model';
 export const signInSchema = z.object({
   email: emailSchema,
   password: passwordSchema,
+  rememberMe: z.boolean().optional(),
 });
 
 export type SignInFormData = z.infer<typeof signInSchema>;
