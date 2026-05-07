4712c0c817745506387756d58377012b6a4822b2
lee0jae330 <xingxing2001@cau.ac.kr>
2026-05-07 15:01:50 +0900

    feat: 내 게시글 페이지 header 추가

---
+++ apps/web/src/widgets/feed/ui/my-feed-header/MyFeedHeader.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ActionHeader } from '@/shared/ui';
+
+export const MyFeedHeader = () => {
+  return (
+    <ActionHeader>
+      <ActionHeader.BackButton>뒤로</ActionHeader.BackButton>
+    </ActionHeader>
+  );
+};
