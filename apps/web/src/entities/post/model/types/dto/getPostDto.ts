969a5b141499aaf264639f0b4ee405149dc2af76
lee0jae330 <xingxing2001@cau.ac.kr>
2026-05-11 02:12:12 +0900

    feat: 게시글 목록 dto에 관리자 계정 여부 필드 추가

--- apps/web/src/entities/post/model/types/dto/getPostDto.ts
+++ apps/web/src/entities/post/model/types/dto/getPostDto.ts
@@ -19,6 +19,7 @@ export interface GetPostResponseDto {
   isPostFavorite: boolean;
   updatable: boolean;
   deletable: boolean;
+  isOfficial: boolean;
   createdAt: string;
   updatedAt: string;
   boardId: string;
