60564b09d9c85be2af0c1425c8b25557c76f4be3
kyh0726 <younghyun75398@gmail.com>
2026-05-14 12:54:51 +0900

    feat: oauth 이후 폼 제출 로직에서 닉네임 및 전화번호 validation 추가

--- apps/web/src/features/auth/ui/oauth-additional-info-nickname-field/OauthAdditionalInfoNicknameField.tsx
+++ apps/web/src/features/auth/ui/oauth-additional-info-nickname-field/OauthAdditionalInfoNicknameField.tsx
@@ -4,13 +4,20 @@ import { OAUTH_ADDITIONAL_INFO_FORM_FIELD } from '@/entities/auth';
 
 import { RHFInput } from '@/shared/ui';
 
-export const OauthAdditionalInfoNicknameField = () => {
+type OauthAdditionalInfoNicknameFieldProps = {
+  onNicknameBlur: () => void;
+};
+
+export const OauthAdditionalInfoNicknameField = ({
+  onNicknameBlur,
+}: OauthAdditionalInfoNicknameFieldProps) => {
   return (
     <RHFInput
       name={OAUTH_ADDITIONAL_INFO_FORM_FIELD.nickname}
       label="닉네임"
       placeholder="닉네임을 입력해주세요."
       typography="body-16-regular"
+      onBlur={onNicknameBlur}
     />
   );
 };
