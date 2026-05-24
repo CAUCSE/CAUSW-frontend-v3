b318dd71d7a0875511733674ef3a8a89b0d41848
kyh0726 <younghyun75398@gmail.com>
2026-05-25 01:03:59 +0900

    feat: og 이미지 추가

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -13,11 +13,35 @@ import {
   GlobalRoutingProvider,
 } from './_provider';
 
+const SOCIAL_PREVIEW_IMAGE = '/images/social-preview.png';
+
 export const metadata: Metadata = {
+  metadataBase: new URL('https://www.causw.co.kr'),
   title: '동문 네트워크',
   description: '동문 네트워크 서비스',
   icons: {
     icon: '/images/favicon-128.png',
+  },
+  openGraph: {
+    title: '동문 네트워크',
+    description: '동문 네트워크 서비스',
+    siteName: '동문 네트워크',
+    images: [
+      {
+        alt: '동문 네트워크 공유 미리보기 이미지',
+        height: 630,
+        url: SOCIAL_PREVIEW_IMAGE,
+        width: 1200,
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    description: '동문 네트워크 서비스',
+    images: [SOCIAL_PREVIEW_IMAGE],
+    title: '동문 네트워크',
   },
   other: {
     ...getTraceData(), // Sentry 오류 로그 추적
