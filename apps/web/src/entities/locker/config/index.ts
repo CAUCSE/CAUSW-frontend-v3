ff308f3669d7be0e4726220b13e3cd2313835fb9
lee0jae330 <xingxing2001@cau.ac.kr>
2026-06-23 16:39:19 +0900

    feat: 사물함 상태 관련 상수 추가

--- apps/web/src/entities/locker/config/index.ts
+++ apps/web/src/entities/locker/config/index.ts
@@ -1,3 +1,5 @@
 export { LOCKER_API_PREFIX } from './lockerApiPrefix';
 export { lockerQueryKeys } from './query-key';
 export { lockerQueryOptions } from './query-options';
+export { LOCKER_STATUS } from './lockerStatus';
+export { LOCKER_STATUS_LEGEND } from './lockerStatusLegend';
