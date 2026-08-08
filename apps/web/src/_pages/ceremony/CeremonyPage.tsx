f610efb73bc923a213fabc85ebd9fd9b628e269c
fresw-woo <sangwooheee@gmail.com>
2026-08-08 19:16:40 +0900

    fix: 경조사 max-width 적용

--- apps/web/src/_pages/ceremony/CeremonyPage.tsx
+++ apps/web/src/_pages/ceremony/CeremonyPage.tsx
@@ -59,14 +59,16 @@ export const CeremonyPage = () => {
         </ActionHeader.ActionButton>
       </ActionHeader>
 
-      <CeremonyListView
-        filter={filter}
-        onFilterChange={setFilter}
-        myStateFilter={myStateFilter}
-        onMyStateFilterChange={setMyStateFilter}
-        onItemClick={handleItemClick}
-        onMyItemClick={handleMyItemClick}
-      />
+      <div className="mx-auto w-full max-w-[900px]">
+        <CeremonyListView
+          filter={filter}
+          onFilterChange={setFilter}
+          myStateFilter={myStateFilter}
+          onMyStateFilterChange={setMyStateFilter}
+          onItemClick={handleItemClick}
+          onMyItemClick={handleMyItemClick}
+        />
+      </div>
 
       <div className="fixed right-[1rem] bottom-[2.75rem]">
         <FloatingActionButton
