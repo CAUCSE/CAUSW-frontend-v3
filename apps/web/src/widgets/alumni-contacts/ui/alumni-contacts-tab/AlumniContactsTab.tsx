141400c8a284a9de3131c784c42f93710eecdecd
Sowon Kim <sso626262@naver.com>
2026-08-24 13:49:57 +0900

    chore: 동문수첩 탭 수정

--- apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-tab/AlumniContactsTab.tsx
+++ apps/web/src/widgets/alumni-contacts/ui/alumni-contacts-tab/AlumniContactsTab.tsx
@@ -33,7 +33,7 @@ export const AlumniContactsTab = () => {
       variant="plain"
       value={currentTab}
       onValueChange={handleTabChange}
-      className="px-1 py-2 md:pt-0"
+      className="px-1 py-2 md:px-0 md:pt-0"
     >
       <Tab.List>
         <Tab.TabItem value={ALUMNI_CONTACTS_TAB.ALUMNI}>동문수첩</Tab.TabItem>
