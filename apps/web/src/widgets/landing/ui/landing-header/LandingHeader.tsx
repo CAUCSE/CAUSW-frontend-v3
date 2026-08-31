77c6930a070a8f26607563cbafd43c4fc3461099
lee0jae330 <xingxing2001@cau.ac.kr>
2026-08-31 21:04:14 +0900

    refactor: 랜딩 헤더 웹사이트 링크 적용

--- apps/web/src/widgets/landing/ui/landing-header/LandingHeader.tsx
+++ apps/web/src/widgets/landing/ui/landing-header/LandingHeader.tsx
@@ -1,16 +1,20 @@
-import { Button, Text } from '@causw/cds';
+import Link from 'next/link';
+
+import { Text } from '@causw/cds';
 
 export const LandingHeader = () => {
   return (
     <header className="max-w-desktop fixed top-0 z-10 flex h-14 w-full items-center justify-end bg-white px-2.25">
-      <Button
-        type="button"
+      <Link
+        href="./auth/sign-in"
+        target="_blank"
+        rel="noopener noreferrer"
         className="rounded-sm bg-gray-700 px-3 py-2 hover:bg-gray-700!"
       >
         <Text typography="body-14-medium" textColor="white" className="w-25.5">
           웹 사이트 이용하기
         </Text>
-      </Button>
+      </Link>
     </header>
   );
 };
