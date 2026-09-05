c63437522e043fe85bddde2744b5ac2eeabbd194
MinSeokKim <kimjunseok23@gmail.com>
2026-09-06 01:41:28 +0900

    refactor: 내비게이션 항목 모델을 새 디자인 구성으로 교체

--- apps/web/src/widgets/navigation-layout/model/navItems.tsx
+++ apps/web/src/widgets/navigation-layout/model/navItems.tsx
@@ -1,69 +1,52 @@
-import { Bell, Board, Book, Contacts, Home, Pen, Setting } from '@causw/cds';
+import { Bell, Board, Contacts, Home, Setting } from '@causw/cds';
 
 import { ROUTES } from '@/shared/constants';
 
 import { type BottomNavItem, type SidebarItem } from './types';
 
 export const SIDEBAR_MAIN_ITEMS: SidebarItem[] = [
-  { key: 'home', label: '홈', icon: <Home />, href: '/home' },
-  {
-    key: 'feed',
-    label: '커뮤니티',
-    icon: <Board size={18} />,
-    href: `${ROUTES.FEED}`,
-  },
-  {
-    key: 'write',
-    label: '글쓰기',
-    icon: <Pen size={18} />,
-    href: `${ROUTES.REGISTER_FEED}`,
-  },
+  { key: 'home', label: '홈', icon: <Home />, href: ROUTES.HOME },
+  { key: 'feed', label: '소식', icon: <Board />, href: ROUTES.FEED },
   {
     key: 'alumni-contacts',
     label: '동문수첩',
-    icon: <Contacts size={18} />,
-    href: `${ROUTES.ALUMNI_CONTACTS}`,
-    activeHrefs: [`${ROUTES.COMMUNITY}`],
+    icon: <Contacts />,
+    href: ROUTES.ALUMNI_CONTACTS,
+    activeHrefs: [ROUTES.COMMUNITY, ROUTES.PROFILE],
   },
   {
-    key: 'profile',
-    label: '내 동문수첩',
-    icon: <Book size={18} />,
-    href: `${ROUTES.PROFILE}`,
+    key: 'setting',
+    label: '내 정보',
+    icon: <Setting />,
+    href: ROUTES.SETTING,
   },
 ];
 
 export const SIDEBAR_BOTTOM_ITEMS: SidebarItem[] = [
-  // {
-  //   key: 'info',
-  //   label: '크자회 소개',
-  //   icon: <Question size={18} />,
-  //   href: '/info',
-  // },
   {
     key: 'notifications',
     label: '알림',
-    icon: <Bell size={18} />,
-    href: `${ROUTES.NOTIFICATION}`,
+    icon: <Bell />,
+    href: ROUTES.NOTIFICATION,
   },
 ];
 
 export const SIDEBAR_ITEMS = [...SIDEBAR_MAIN_ITEMS, ...SIDEBAR_BOTTOM_ITEMS];
 
 export const BOTTOM_NAV_ITEMS: BottomNavItem[] = [
-  { key: 'home', label: '홈', icon: <Home />, href: `${ROUTES.HOME}` },
-  { key: 'feed', label: '커뮤니티', icon: <Board />, href: `${ROUTES.FEED}` },
+  { key: 'home', label: '홈', icon: <Home />, href: ROUTES.HOME },
+  { key: 'feed', label: '소식', icon: <Board />, href: ROUTES.FEED },
   {
     key: 'alumni-contacts',
     label: '동문수첩',
     icon: <Contacts />,
-    href: `${ROUTES.ALUMNI_CONTACTS}`,
-    activeHrefs: [`${ROUTES.COMMUNITY}`],
+    href: ROUTES.ALUMNI_CONTACTS,
+    activeHrefs: [ROUTES.COMMUNITY],
   },
   {
     key: 'setting',
     label: '내 정보',
     icon: <Setting />,
-    href: `${ROUTES.SETTING}`,
+    href: ROUTES.SETTING,
   },
 ];
