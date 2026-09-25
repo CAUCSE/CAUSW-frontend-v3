e9cd4cec139a59b77e96c317793fee591e7adbb7
clue31415 <clue31415@gmail.com>
2026-09-25 19:33:24 +0900

    Delete apps/web/src/entities/post/model/queries/usePostRefetch.ts

--- apps/web/src/entities/post/model/queries/usePostRefetch.ts
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useSuspenseQuery } from '@tanstack/react-query';
-
-import { postQueryOptions } from '../../config';
-
-export const usePostRefetch = (postId: string) => {
-  return useSuspenseQuery(postQueryOptions.refetch(postId));
-};
